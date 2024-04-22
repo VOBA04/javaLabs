2ed9331bfb3ebbb8744dd9c0445fc43f733cd57f
VOBA04 <vova.bolashenko@gmail.com>
2024-04-22 11:36:03 +0300

    exception handler tests

---
+++ src/test/java/com/vova/laba/exceptions/ExceptionHabdlerTests.java
@@ -0,0 +1,60 @@
+package com.vova.laba.exceptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+@ExtendWith(MockitoExtension.class)
+class ExceptionHabdlerTests {
+
+  @InjectMocks private ExceptionHandlerController exceptionHandler;
+
+  @Test
+  void testHandleBadRequestException() {
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    WebRequest webRequest = new ServletWebRequest(request);
+    ResponseEntity<ExceptionDetails> result =
+        exceptionHandler.badRequest(new BadRequestException("Test"), webRequest);
+
+    assertEquals("Test", result.getBody().getMessage());
+    assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+  }
+
+  @Test
+  void testHandlerNotFoundException() {
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    WebRequest webRequest = new ServletWebRequest(request);
+    ResponseEntity<ExceptionDetails> result =
+        exceptionHandler.notFound(new NotFoundExcepcion("Test"), webRequest);
+    assertEquals("Test", result.getBody().getMessage());
+    assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
+  }
+
+  @Test
+  void testHandlerApiException() {
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    WebRequest webRequest = new ServletWebRequest(request);
+    ResponseEntity<ExceptionDetails> result =
+        exceptionHandler.apiException(new ApiException("Test"), webRequest);
+    assertEquals("Test", result.getBody().getMessage());
+    assertEquals(HttpStatus.SERVICE_UNAVAILABLE, result.getStatusCode());
+  }
+
+  @Test
+  void testHandlerInternalServiceException() {
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    WebRequest webRequest = new ServletWebRequest(request);
+    ResponseEntity<ExceptionDetails> result =
+        exceptionHandler.internalServiceException(new Exception("Test"), webRequest);
+    assertEquals("Test", result.getBody().getMessage());
+    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.getStatusCode());
+  }
+}
