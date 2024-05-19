f925e7393eedf5acd3f16dc31da5d0ecd4e1429a
VOBA04 <vova.bolashenko@gmail.com>
2024-05-19 17:33:27 +0300

    8

--- src/main/java/com/vova/laba/LabaApplication.java
+++ src/main/java/com/vova/laba/LabaApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LabaApplication {
 
-  
   public static void main(String[] args) {
     SpringApplication.run(LabaApplication.class, args);
   }
